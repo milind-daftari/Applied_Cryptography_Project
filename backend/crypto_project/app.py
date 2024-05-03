0edb241dece6ccabd4749fd09de6457e815fba65
Harshit Rajpal <hexisanoob@gmail.com>
2024-05-03 14:08:28 -0400

    imporvement in data search

--- backend/crypto_project/app.py
+++ backend/crypto_project/app.py
@@ -15,10 +15,11 @@ def setup():
     datafolder = 'demoData'
     serType = BINARY  # BINARY or JSON
 
-    if not os.path.exists(datafolder):
-        os.makedirs(datafolder)
-        print(f"Created directory `{datafolder}` for storing data.")
-    
+    # Ensure the data directory exists
+    os.makedirs(datafolder, exist_ok=True)
+    print(f"Created or verified directory `{datafolder}` for storing data.")
+
+    # Initialize Crypto Context and Key Generation
     parameters = CCParamsBFVRNS()
     parameters.SetPlaintextModulus(65537)
     parameters.SetMultiplicativeDepth(2)
@@ -28,9 +29,28 @@ def setup():
     cryptoContext.Enable(PKESchemeFeature.KEYSWITCH)
     cryptoContext.Enable(PKESchemeFeature.LEVELEDSHE)
 
+    if not SerializeToFile(datafolder + "/cryptocontext.txt", cryptoContext, serType):
+        raise Exception(
+            "Error writing serialization of the crypto context to cryptocontext.txt")
+    print("The cryptocontext has been serialized.")
+
     keypair = cryptoContext.KeyGen()
+    if not keypair:
+        raise Exception("Key generation failed.")
+
+    SerializeToFile(datafolder + "/key-public.txt", keypair.publicKey, serType)
+    SerializeToFile(datafolder + "/key-private.txt",
+                    keypair.secretKey, serType)
+
     cryptoContext.EvalMultKeyGen(keypair.secretKey)
+    cryptoContext.SerializeEvalMultKey(
+        datafolder + "/key-eval-mult.txt", serType)
     cryptoContext.EvalRotateKeyGen(keypair.secretKey, [1, 2, -1, -2])
+    cryptoContext.SerializeEvalAutomorphismKey(
+        datafolder + "/key-eval-rot.txt", serType)
+
+    print("Keys and evaluation keys have been serialized.")
+
     encrypt_and_store([1, 2, 3, 4, 5, 6, 7, 8])
 
 def encrypt_and_store(values):
@@ -40,6 +60,14 @@ def encrypt_and_store(values):
         ciphertext = cryptoContext.Encrypt(keypair.publicKey, plaintext)
         filename = f"{datafolder}/ciphertext{idx}.txt"
         SerializeToFile(filename, ciphertext, serType)
+    create_dbtxt(values)
+
+def create_dbtxt(values):
+    fn = f"{datafolder}/db.txt"
+    with open(fn, 'w') as file:
+        for idx in values:
+            file.write(f"ciphertext{idx}.txt")
+            file.write("\n")
 
 def encrypt_input(value):
     plaintext = cryptoContext.MakePackedPlaintext([int(value)])
@@ -88,4 +116,5 @@ def filter_options():
 
 if __name__ == '__main__':
     setup()
+
     app.run(host='0.0.0.0', debug=True)
